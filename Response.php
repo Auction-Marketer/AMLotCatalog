3c469f5f77fa76a7a8b82acd59e991fe8e550769
Chris Cuber <k.cuber@braunmann.com>
2021-09-10 09:49:11 +0200

    Fix for unhandled exceptions

--- Response.php
+++ Response.php
@@ -11,9 +11,16 @@ abstract class Response extends Record {
         if(gettype($response_data) === 'string') {
             $response_data = json_decode($response_data, true);
         }
-        $this->setDataType($response_data['dataType']);
-        $this->setSuccess($response_data['success']);
-        $this->setData($response_data['data']);
+        try {
+            if(!isset($response_data['success']) or $response_data['success'] == false) {
+                throw new \Exception('No results found');
+            }
+            $this->setDataType($response_data['dataType']);
+            $this->setSuccess($response_data['success']);
+            $this->setData($response_data['data']);
+        } catch (\Exception $e) {
+            $this->setSuccess(false);
+        }
     }
 
     abstract protected function setData($data);
